5919192ff5ede59b6d18bdf0d8be6dada06eb4d2
WilliamChanthavong <121891997+WilliamChanthavong@users.noreply.github.com>
2023-10-01 20:18:28 -0700

    Add files via upload

--- quiz_game_page.js
+++ quiz_game_page.js
@@ -1,9 +1,42 @@
+plr1score = document.getElementById("player1_score").value;
+plr2score = document.getElementById("player2_score").value;
+
+plr1actualscore = 0;
+plr2actualscore = 0;
+
+plr1score = plr1actualscore;
+plr2score = plr2actualscore;
+
 function send()
 {
-    playernumber1 = document.getElementById("number_1").value;
-    playernumber2 = document.getElementById("number_2").value;
-    localStorage.setItem("NumberOne", playernumber1);
-    localStorage.setItem("NumberTwo", playernumber2);
-    // next part below
-    // window.location = "nextpage.html";
+    num1 = document.getElementById("number_1").value;
+    num2 = document.getElementById("number_2").value;
+    localStorage.setItem("NumberOne", num1);
+    localStorage.setItem("NumberTwo", num2);
+    actual_anwser = parseInt(num1) * parseInt(num2);
+    question_num = "<h4>" + num1 + " X "+ num2 + "</h4>";
+    input_box = "<br>Answer : <input type='text' id='input_check_box'>";
+    check_btn = "<br><button class='btn btn-info' onclick='check()'>Check</button>";
+    row=question_num + input_box + check_btn;
+    document.getElementById("output").innerHTML = row;
+    document.getElementById("number_1").value = "";
+    document.getElementById("number_2").value = "";
+    localStorage.setItem("Answer", actual_anwser);
+}
+
+function check()
+{
+    answer = localStorage.getItem("Answer");
+    text_answer = document.getElementById("input_check_box").value;
+
+    if(text_answer=answer)
+    {
+        plr2actualscore = plr2actualscore+1;
+        plr2score = plr2actualscore;
+    }
+    else
+    {
+        plr1actualscore = plr1actualscore+1;
+        plr1score = plr1actualscore;
+    }
 }
