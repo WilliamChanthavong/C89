9039fab8793cfbf5069533f57d5141951c3afd4f
WilliamChanthavong <121891997+WilliamChanthavong@users.noreply.github.com>
2023-10-15 20:04:36 -0700

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -18,7 +18,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
